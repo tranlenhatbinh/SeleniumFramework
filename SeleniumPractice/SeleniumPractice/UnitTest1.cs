b14d46c1b26d5563bbe5b5a4b4a5e21813100efe
tranlenhatbinh <tranlenhatbinh191988@gmail.com>
2017-02-16 09:40:43 +0700

    hi

---
+++ SeleniumPractice/SeleniumPractice/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeleniumPractice
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //hi
+        }
+    }
+}
