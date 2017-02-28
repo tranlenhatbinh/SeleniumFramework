a412e156ccc88e493f7067932552b0514864d37c
trinhnguyen <trinhnguyen0502@gmail.com>
2017-02-28 17:28:07 +0700

    Implement AdvanceSearch Page action

--- SeleniumPractice/SeleniumPractice/PageObject/AdvanceSearch.cs
+++ SeleniumPractice/SeleniumPractice/PageObject/AdvanceSearch.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
 
 namespace SeleniumPractice.PageObject
 {
-    class AdvanceSearch
+    public class AdvanceSearch : GeneralPage
     {
+        IWebDriver driver;
+
+        public  AdvanceSearch (IWebDriver driver)
+        {
+            this.driver = driver;
+        }
     }
 }
