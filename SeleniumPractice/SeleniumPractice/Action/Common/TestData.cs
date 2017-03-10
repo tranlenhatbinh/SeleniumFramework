0e73ad8424f3d895ae5ac2050dea00400cb290e1
Administrator <Administrator@ADMINISTRATOR>
2017-03-10 21:57:23 +0700

    1

--- SeleniumPractice/SeleniumPractice/Action/Common/TestData.cs
+++ SeleniumPractice/SeleniumPractice/Action/Common/TestData.cs
@@ -8,5 +8,7 @@ namespace SeleniumPractice.Action.Common
         public static string ebscodURL = ConfigurationManager.AppSettings["url"];
         public static string searchTerm = "test";
         public static string runtype = ConfigurationManager.AppSettings["run type"];
+        private static string FirefoxVersion = ConfigurationManager.AppSettings["FirefoxVersion"];
+        private static string firefoxPlatform = ConfigurationManager.AppSettings["FirefoxPlatform"];
     }
 }
